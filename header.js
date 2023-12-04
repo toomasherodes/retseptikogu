1cb4be740222c9e81a5d30d15f511f8eccb9ed78
toomas <herodes.toomas@gmail.com>
2023-12-04 20:52:12 +0200

    Kommentaaride täpsustused JS failides

--- header.js
+++ header.js
@@ -1,6 +1,6 @@
 /*
 Javascript fail, mis lisab lehele headeri ja footeri.
-Headeri ja footeri stiilid määrab global.css
+Headeri ja footeri stiilid määrab global.css.
 */
 
 // div klassinimega "header"
@@ -8,26 +8,22 @@ var x = document.createElement("div");
 x.setAttribute("class", "header");
 
 // pildi kaustast "pictures" klassinime ja id-ga "logo" lisamine headerisse
-var y = document.createElement("img")
-y.setAttribute("src", "./pictures/logo.png")
-y.setAttribute("class", "logo")
-y.setAttribute("id", "logo")
-x.appendChild(y)
+var y = document.createElement("img");
+y.setAttribute("src", "./pictures/logo.png");
+y.setAttribute("class", "logo");
+y.setAttribute("id", "logo");
+x.appendChild(y);
 
 // headeri lisamine lehe algusesse
-document.body.insertBefore(x, document.body.firstChild)
+document.body.insertBefore(x, document.body.firstChild);
 
-// footeri klassinimega "footer" ja sisuga "2023" lisamine lehe lõppu 
-document.body.innerHTML += '<div class="footer"> 2023 </div>'
+// footeri klassinimega "footer" ja sisuga "2023" lisamine lehe lõppu
+document.body.innerHTML += '<div class="footer"> 2023 </div>';
 
-// logo klikkimisel suunatamine esilehele
-const u = document.getElementById("logo")
-u.addEventListener("click", function(i) {
-    window.location.replace("./index.html");
-  });    
-
-
-
-
+// logo klikkimisel suunamine esilehele
+var u = document.getElementById("logo");
+u.addEventListener("click", function (i) {
+  window.location.replace("./index.html");
+});
 
 // koodi autor: Toomas Herodes
--- recipeMenu.js
+++ recipeMenu.js
@@ -5,12 +5,13 @@ failist data.json leitud vasted lehele recipeMenu.html
 
 
 async function insertContent() {
+    // Parameetri "search" ja data.json andmete leidmine 
     const params = new URLSearchParams(window.location.search);
     const search = params.get('search');
-
     const jsonData = await fetch('./data.json')
     const data = await jsonData.json()
 
+    // Otsingusõna leidmine retseptidest
     result = []
     for (x in data){
         if(data[x].Valmistamine.toLowerCase().includes(search.toLowerCase()) || data[x].Kategooria.toLowerCase().includes(search.toLowerCase())){
@@ -18,6 +19,7 @@ async function insertContent() {
         }
     }
 
+    // Leitud retseptide lisamine addHTML muutujasse
     addHTML = ""
     result.forEach(element => {
         addHTML += `
@@ -32,11 +34,12 @@ async function insertContent() {
         </div>
         `
     });
+    // addHTML muutuja sisu lisamine lehele. Kui see on tühi, lisatakse teade retseptide mitte leidmise kohta
     if(addHTML != ""){document.getElementById("main_container").innerHTML += addHTML}
     else{document.getElementById("main_container").innerHTML += '<h1 class="notFoundText">Selle märksõnaga retsepte ei leitud</h1>'}
 }
 
-// Retsepti lisamine pärast lehe laadimist
+// Retseptide otsimine ja lisamine pärast lehe laadimist
 Window.onload = insertContent();
 
 // koodi autor: Toomas Herodes
